221b61ec990ffc4f6ed652eca9267cd28f7ca849
Hasibullah Aman <hasib.aman7@gmail.com>
2023-07-20 07:05:34 +0430

    make some update to knn file

--- classfication-knn.py
+++ classfication-knn.py
@@ -4,3 +4,10 @@ import numpy as np
 class NearistNighbors:
     def __init__(self):
         pass
+
+    def train(self, X,y):
+        self.Xtr = X
+        self.ytr = y
+
+    def predict(self,X):
+        pass
