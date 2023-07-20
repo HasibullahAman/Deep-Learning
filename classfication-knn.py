c31355a9356e45f5b49dc485e60987a5b2791599
Hasibullah Aman <hasib.aman7@gmail.com>
2023-07-20 07:33:02 +0430

    done createing Nearist nighbors

--- classfication-knn.py
+++ classfication-knn.py
@@ -10,4 +10,12 @@ class NearistNighbors:
         self.ytr = y
 
     def predict(self,X):
-        pass
+        num_test = X.shape[0]
+        ypred = np.zeros(num_test,dtype=self.ytr.dtype)
+
+
+        for i in xrange(num_test):
+            distence = np.sum(np.abs(self.Xtr - X[i,:]),axis=1)
+            min_index = np.argmin(distence)
+            ypred[i] = self.ytr[min_index]
+            return ypred
